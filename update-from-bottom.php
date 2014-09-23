8e1c8cf1eb712ab00d3b16aa6a9aba56e2425f97
urre <urre@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-23 10:16:17 +0000

    tagging version 1.0.1
    git-svn-id: https://plugins.svn.wordpress.org/update-from-bottom/tags/1.0.1@995321 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ update-from-bottom.php
@@ -0,0 +1,85 @@
+<?php
+/*
+Plugin Name: Update from Bottom
+Plugin URI: http://labs.urre.me
+Description: Show two extra buttons (Scroll to top and Publish/Update) in the bottom of the screen when user scrolls near bottom. Suitable for posts and pages with a lot of meta boxes, or when edit.php tends to get very long.
+Version: 1.0.1
+Author: Urban Sanden
+Author URI: http://urre.me
+Author Email: hej@urre.me
+License: GPL2
+*/
+
+/*  Copyright 2104 Urban Sanden (email: hej@urre.me)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+class UpdatefromBottom {
+
+    function __construct() {
+
+        # Load plugin text domain
+        add_action( 'init', array( $this, 'plugin_textdomain' ) );
+
+        # Register admin styles and scripts
+        add_action( 'admin_print_styles', array( $this, 'register_admin_styles' ) );
+        add_action( 'admin_enqueue_scripts', array( $this, 'register_admin_scripts' ) );
+
+    }
+
+    public function plugin_textdomain() {
+        $domain = 'updatefrombottom';
+        $locale = apply_filters( 'plugin_locale', get_locale(), $domain );
+        load_textdomain( $domain, WP_LANG_DIR.'/'.$domain.'/'.$domain.'-'.$locale.'.mo' );
+        load_plugin_textdomain( $domain, FALSE, dirname( plugin_basename( __FILE__ ) ) . '/lang/' );
+    }
+
+    public function register_admin_styles() {
+
+        # Don't show buttons on main dashboard page, i.e enqueue any styles
+        if( 'index.php' == $hook )
+               return;
+
+        if (is_admin()) {
+            wp_enqueue_style( 'updatefrombottom-plugin-styles', plugins_url( 'update-from-bottom/css/update-from-bottom.admin.css' ) );
+        }
+    }
+
+    public function register_admin_scripts() {
+
+        # Don't show buttons on main dashboard page, i.e enqueue any scripts
+        if( 'index.php' == $hook )
+               return;
+
+        # Enqueue script
+        wp_enqueue_script( 'updatefrombottom-admin-script', plugins_url( 'update-from-bottom/js/update-from-bottom.admin.js' ), array('jquery') );
+
+        # Localize strings to js
+        $js_data = array(
+            'update'     => __( 'Update', 'updatefrombottom' ),
+            'publish'    => __( 'Publish', 'updatefrombottom' ),
+            'publishing' => __( 'Publishing...', 'updatefrombottom' ),
+            'updating'   => __( 'Updating...', 'updatefrombottom' ),
+            'totop'      => __( 'To top', 'updatefrombottom' ),
+        );
+
+        wp_localize_script('updatefrombottom-admin-script', 'updatefrombottomParams', $js_data);
+
+    }
+
+
+}
+
+$ufb = new UpdatefromBottom();
